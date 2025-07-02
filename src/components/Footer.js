88458de84fb76aca3d362a172f001da726fdca2d
s210935 <s210935@th-ab.de>
2025-07-02 06:20:42 +0200

    Final commit for personal website

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/anhthu-logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.png";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={8}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={4} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit"><img src={navIcon1} alt="Icon" /></a>
+              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://github.com/anhthupham16"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2025. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
