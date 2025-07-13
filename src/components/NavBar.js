de7d0d0b585629b6c161ea8a7c4e3fc6199e4af0
s210935 <s210935@th-ab.de>
2025-07-13 19:10:55 +0200

    Menu Hamburger for Mobil

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,6 +11,7 @@ export const NavBar = () => {
 
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
@@ -28,17 +29,24 @@ export const NavBar = () => {
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
+    // Khi click vào link, đóng navbar lại (tùy chọn, nhưng thường là hành vi mong muốn)
+    setExpanded(false);
+  }
+  // Hàm riêng để đóng menu khi click Let's Connect
+  const handleConnectClick = () => {
+    setExpanded(false);
   }
 
   return (
     <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Navbar expand="md" expanded={expanded} onToggle={() => setExpanded(!expanded)} className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
             <img src={logo} alt="Logo" />
           </Navbar.Brand>
+          {/* nút đóng tắt nav bả trong màn hình mobile, nút 3 gạch */}
           <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
+            <span className={`navbar-toggler-icon ${expanded ? 'open' : ''}`}></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
@@ -52,7 +60,7 @@ export const NavBar = () => {
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="https://github.com/anhthupham16?tab=repositories"><img src={navIcon3} alt="" /></a>
               </div>
-              <HashLink to='#connect'>
+              <HashLink to='#connect' onClick={handleConnectClick}>
                 <button className="vvd"><span>Let’s Connect</span></button>
               </HashLink>
             </span>
